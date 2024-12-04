59062d5b3c8b2bed1f1bf5218ce1aeecceee804f
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-12-04 20:04:04 +0530

    section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { BannerSectionComponent } from './pages/home/banner-section/banner-section.component';
 import { Section2Component } from './pages/home/section-2/section-2.component';
+import { Section3Component } from './pages/home/section-3/section-3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, BannerSectionComponent, Section2Component],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, BannerSectionComponent, Section2Component, Section3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/home/section-3/section-3.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-3',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-3.component.html',
+  styleUrl: './section-3.component.css'
+})
+export class Section3Component {
+
+  processIcon= 'assets/icon/process.svg'
+
+  timeIcon = 'assets/icon/time.svg';
+  smileIcon = 'assets/icon/smile.svg';
+  analyticsIcon = 'assets/icon/analytics.svg';
+  defenceIcon = 'assets/icon/defence.svg';
+
+  heroLeftSvg = 'assets/svg/hero-left.svg';
+  heroRightSvg = 'assets/svg/hero-right.svg';
+}
