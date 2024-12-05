70bb39b946ca2066da5318f9c89043f570a99836
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-12-06 02:15:24 +0530

    project full section

--- src/app/pages/projects/section-2/section-2.component.ts
+++ src/app/pages/projects/section-2/section-2.component.ts
@@ -9,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class Section2Component {
 
+  buildingIcon = 'assets/icon/building.svg'
+
+  projectImg1 = 'assets/projects/project-img1.png'
+  projectImg2 = 'assets/projects/project-img2.png'
+  projectImg3 = 'assets/projects/project-img3.png'
+  projectImg4 = 'assets/projects/project-img4.png'
+  projectImg5 = 'assets/projects/project-img5.png'
+  projectImg6 = 'assets/projects/project-img6.png'
+  projectImg7 = 'assets/projects/project-img7.png'
+  projectImg8 = 'assets/projects/project-img8.png'
+
+  projectSvg1 = 'assets/icon/project-svg1.svg'
+  projectSvg2 = 'assets/icon/project-svg2.svg'
+  projectSvg3 = 'assets/icon/project-svg3.svg'
+  projectSvg4 = 'assets/icon/project-svg4.svg'
+  projectSvg5 = 'assets/icon/project-svg5.svg'
+  projectSvg6 = 'assets/icon/project-svg6.svg'
+  projectSvg7 = 'assets/icon/project-svg7.svg'
+  projectSvg8 = 'assets/icon/project-svg8.svg'
+  projectSvg9 = 'assets/icon/project-svg9.svg'
 }
