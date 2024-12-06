ff5ea2e0886ac772fbf65cfe82601ede0880bc14
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-12-06 16:59:49 +0530

    last sec

--- src/app/pages/investment/section-4/section-4.component.ts
+++ src/app/pages/investment/section-4/section-4.component.ts
@@ -9,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class Section4Component {
 
+  signIcon = 'assets/icon/sign.svg'
+  ivestmentIcon3 = 'assets/icon/investment-icon3.svg'
+  ivestmentIcon4 = 'assets/icon/investment-icon4.svg'
+  ivestmentIcon5 = 'assets/icon/investment-icon5.svg'
+
+
+  projectImg1 = 'assets/projects/project-img1.png'
+  projectImg2 = 'assets/projects/project-img2.png'
+  projectImg3 = 'assets/projects/project-img3.png'
+  projectImg4 = 'assets/projects/project-img4.png'
+  projectImg5 = 'assets/projects/project-img5.png'
+  projectImg6 = 'assets/projects/project-img6.png'
+  projectImg7 = 'assets/projects/project-img7.png'
+  projectImg8 = 'assets/projects/project-img8.png'
 }
