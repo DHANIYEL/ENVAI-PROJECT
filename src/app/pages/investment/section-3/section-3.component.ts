882b87a0ba2ecc071f3bfba9ce2f3472591415b4
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-12-06 16:43:38 +0530

    section 3 investment

--- src/app/pages/investment/section-3/section-3.component.ts
+++ src/app/pages/investment/section-3/section-3.component.ts
@@ -9,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class Section3Component {
 
+  rulesIcon = 'assets/icon/token-icon5.svg'
+
+  tokenIcon1 = 'assets/icon/token-icon1.svg'
+  tokenIcon2 = 'assets/icon/token-icon2.svg'
+  tokenIcon3 = 'assets/icon/token-icon3.svg'
+  tokenIcon4 = 'assets/icon/token-icon4.svg'
+
+
+  projectImg1 = 'assets/projects/project-img1.png'
+  projectImg2 = 'assets/projects/project-img2.png'
+  projectImg3 = 'assets/projects/project-img3.png'
+  projectImg4 = 'assets/projects/project-img4.png'
+  projectImg5 = 'assets/projects/project-img5.png'
+  projectImg6 = 'assets/projects/project-img6.png'
+  projectImg7 = 'assets/projects/project-img7.png'
+  projectImg8 = 'assets/projects/project-img8.png'
 }
