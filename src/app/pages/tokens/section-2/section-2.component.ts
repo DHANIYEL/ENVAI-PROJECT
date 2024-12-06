2fc5f8739db5eb15b2723669d085792b1a1abca0
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-12-06 14:43:05 +0530

    line alighnments

--- src/app/pages/tokens/section-2/section-2.component.ts
+++ src/app/pages/tokens/section-2/section-2.component.ts
@@ -9,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class Section2Component {
 
+  bannerImg = 'assets/tokens/token-banner.jpeg';
+
+  securityIcon = 'assets/icon/security.svg'
+  closeAroowIcon = 'assets/icon/close-arrow.svg'
+
+  tokenIcon5 = 'assets/icon/token-icon5.svg'
+  tokenIcon6 = 'assets/icon/token-icon6.svg'
+  tokenIcon7 = 'assets/icon/token-icon7.svg'
+  tokenIcon8 = 'assets/icon/token-icon8.svg'
+  tokenIcon9 = 'assets/icon/token-icon9.svg'
+  tokenIcon10 = 'assets/icon/token-icon10.svg'
+
+  projectImg1 = 'assets/projects/project-img1.png'
+  projectImg2 = 'assets/projects/project-img2.png'
+  projectImg3 = 'assets/projects/project-img3.png'
+  projectImg4 = 'assets/projects/project-img4.png'
+  projectImg5 = 'assets/projects/project-img5.png'
+  projectImg6 = 'assets/projects/project-img6.png'
+  projectImg7 = 'assets/projects/project-img7.png'
+  projectImg8 = 'assets/projects/project-img8.png'
 }
--- src/app/pages/tokens/tokens.component.ts
+++ src/app/pages/tokens/tokens.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Section1Component } from "./section-1/section-1.component";
 import { Section2Component } from "./section-2/section-2.component";
+import { JoinSectionComponent } from "../../common/join-section/join-section.component";
+import { NewsletterComponent } from "../../common/newsletter/newsletter.component";
 
 @Component({
   selector: 'app-tokens',
   standalone: true,
-  imports: [Section1Component, Section2Component],
+  imports: [Section1Component, Section2Component, JoinSectionComponent, NewsletterComponent],
   templateUrl: './tokens.component.html',
   styleUrl: './tokens.component.css'
 })
