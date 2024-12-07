70b9a27b601eacfa6ec04301e5f036fec4f379f0
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-12-08 01:38:15 +0530

    get started navigation

--- src/app/common/join-section/join-section.component.ts
+++ src/app/common/join-section/join-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-join-section',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './join-section.component.html',
   styleUrl: './join-section.component.css'
 })
