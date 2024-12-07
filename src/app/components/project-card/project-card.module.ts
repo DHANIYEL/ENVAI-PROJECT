a78474ea1d24f6cd9f3f50edb1e92945d2a8d46d
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-12-07 12:01:26 +0530

    invest navigation

--- src/app/components/project-card/project-card.module.ts
+++ src/app/components/project-card/project-card.module.ts
@@ -1,11 +1,15 @@
 // src/app/components/project-card/project-card.module.ts
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule for common Angular directives (ngIf, ngFor, etc.)
-import { ProjectCardComponent } from './project-card.component'; // Import the ProjectCardComponent
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Import RouterModule
+import { ProjectCardComponent } from './project-card.component';
 
 @NgModule({
-  declarations: [ProjectCardComponent],  // Declare the ProjectCardComponent in this module
-  imports: [CommonModule],  // Import CommonModule if you're using Angular's common directives
-  exports: [ProjectCardComponent]  // Export the ProjectCardComponent so it can be used in other modules
+  declarations: [ProjectCardComponent],
+  imports: [
+    CommonModule,
+    RouterModule // Add RouterModule here
+  ],
+  exports: [ProjectCardComponent]
 })
 export class ProjectCardModule {}
