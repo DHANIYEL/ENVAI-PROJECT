42628348fa28de88ad4e257ae8619bc5134a7f83
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-12-25 01:26:54 +0530

    carousal of project card

--- src/app/pages/home/section-4/section-4.component.ts
+++ src/app/pages/home/section-4/section-4.component.ts
@@ -1,20 +1,22 @@
 import { Component, NgModuleFactory } from '@angular/core';
 import { ProjectCardModule } from '../../../components/project-card/project-card.module'; // Import the new module
-import { NgForOf, NgIf } from '@angular/common';
+import { CommonModule, NgForOf, NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router'; // Import RouterModule for routerLink
 
 
 @Component({
   selector: 'app-section-4',
   standalone: true,
-  imports: [ProjectCardModule, NgForOf, RouterModule], // Import the module (not the component directly anymore)
+  imports: [ProjectCardModule, NgForOf, RouterModule, CommonModule], // Import the module (not the component directly anymore)
   templateUrl: './section-4.component.html',
   styleUrls: ['./section-4.component.css'],
 
 })
 export class Section4Component {
-  statementImg = 'assets/icon/projects.svg'; // Example image for Section 4
 
+  currentSlide = 0;
+
+  statementImg = 'assets/icon/projects.svg'; // Example image for Section 4
   projects = [
     {
       name: 'sample',
